3edf5bf1c5c8d84e14f684b78c15f90e47a2dfdc
qiaoyn <1750718285@qq.com>
2019-11-19 15:00:45 +0800

    test02用来测试。。。。
    Signed-off-by: qiaoyn <1750718285@qq.com>

---
+++ src/main/java/com/qyn/demo01/test02.java
@@ -0,0 +1,11 @@
+package com.qyn.demo01;
+
+/**
+ * @author qiaoyn
+ **/
+public class test02 {
+    public static void main(String args[]){
+        System.out.println("今天你吃了啊");
+        System.out.println("今天你吃了啊");
+    }
+}
